f2cec6a0f6817f70cb01e2f6752efe2fedae7083
tatianamp91 <tatianamp91@gmail.com>
2014-11-01 20:56:12 +0000

    [tatianamp91] vista solicitud bandeja

---
+++ PQRS/src/com/tcbuen/pqrs/presentation/backingBeans/CasosAsignadosView.java
@@ -0,0 +1,62 @@
+package com.tcbuen.pqrs.presentation.backingBeans;
+
+import com.tcbuen.pqrs.modelo.*;
+import com.tcbuen.pqrs.modelo.dto.SolicitudDTO;
+import com.tcbuen.pqrs.presentation.businessDelegate.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+
+@ManagedBean
+@ViewScoped
+public class CasosAsignadosView implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private List<SolicitudDTO> data;
+	private SolicitudDTO selectedSolicitudPqr;
+
+	@ManagedProperty(value = "#{BusinessDelegatorView}")
+	private IBusinessDelegatorView businessDelegatorView;
+
+	public CasosAsignadosView() {
+		super();
+	}
+
+	public List<SolicitudDTO> getData() throws Exception {
+		try{
+			AreasInvolucradas area = businessDelegatorView.getAreasInvolucradas(1L);
+			if(area != null){
+				businessDelegatorView.consultarAsignacion(area);
+			}
+		}catch(Exception e){
+			throw new Exception (e);
+		}
+		return data;
+	}
+
+	public void setData(List<SolicitudDTO> data) {
+		this.data = data;
+	}
+
+	public SolicitudDTO getSelectedSolicitudPqr() {
+		return selectedSolicitudPqr;
+	}
+
+	public void setSelectedSolicitudPqr(SolicitudDTO selectedSolicitudPqr) {
+		this.selectedSolicitudPqr = selectedSolicitudPqr;
+	}
+
+	public IBusinessDelegatorView getBusinessDelegatorView() {
+		return businessDelegatorView;
+	}
+
+	public void setBusinessDelegatorView(
+			IBusinessDelegatorView businessDelegatorView) {
+		this.businessDelegatorView = businessDelegatorView;
+	}	
+}
